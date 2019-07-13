19e4cb5c1576337be22ae238e9a9f3e55b05f96f
blitheanon <blitheanon@qq.com>
2019-07-13 10:43:55 +0800

    Feature: 添加空气质量背景颜色匹配

--- js/logo-match.js
+++ js/logo-match.js
@@ -34,3 +34,29 @@ function logoMatch(wea) {
 
     return match;
 }
+
+//空气质量等级颜色匹配
+function airMatch(_level) {
+    let match;
+    switch (_level) {
+        case "良":
+            match = 'level_2';
+            break;
+        case "轻度污染":
+            match = 'level_3';
+            break;
+        case "中度污染":
+            match = 'level_4';
+            break;
+        case "重度污染":
+            match = 'level_5';
+            break;
+        case "严重污染":
+            match = 'level_6';
+            break;
+        default:
+            match = 'level_1';
+    }
+
+    return match;
+}
