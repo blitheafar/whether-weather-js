3aa18b772ff6f5a1ec63cf4443321de505fe641a
blitheanon <blitheanon@qq.com>
2019-07-14 19:46:05 +0800

    Feature: 新增一周天气折线图

--- js/match.js
+++ js/match.js
@@ -1,6 +1,5 @@
 //通过天气匹配图标
 //xue, lei, shachen, wu, bingbao, yun, yu, yin, qing
-
 function logoMatch(wea) {
     let match;
     switch (wea) {
@@ -58,5 +57,64 @@ function airMatch(_level) {
             match = 'level_1';
     }
 
+    return match;
+}
+
+//匹配空气质量富文本
+// `{styleName|text content text content}` 标记样式名。
+//通过index判断rich值
+function airRichMatch(value) {
+    let match;
+    switch (value) {
+        case '良':
+            match = '{level_2|良}';
+            break;
+        case '轻度污染':
+            match = '{level_3|轻度}';
+            break;
+        case '中度污染':
+            match = '{level_4|中度}';
+            break;
+        case '重度污染':
+            match = '{level_5|重度}';
+            break;
+        case '严重污染':
+            match = '{level_6|严重}';
+            break;
+        default:
+            match = '{level_1|优}';
+    }
+
+    return match;
+}
+
+//匹配天气图标富文本
+function weaLogoRichMatch(value) {
+    let match;
+    switch (value) {
+        case "xue":
+            match = '{snow|}';
+            break;
+        case "shachen":
+            match = '{dust|}';
+            break;
+        case "wu":
+            match = '{fog|}';
+            break;
+        case "bingbao":
+            match = '{hail|}';
+            break;
+        case "yun":
+            match = '{cloud|}';
+            break;
+        case "yu":
+            match = '{rain|}';
+            break;
+        case "yin":
+            match = '{cloudy|}';
+            break;
+        default:
+            match = '{sunny|}';
+    }
     return match;
 }
