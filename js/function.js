17f26a16640c555038133dad25ce4132b82aa920
blitheanon <blitheanon@qq.com>
2019-07-15 11:11:38 +0800

    Feature: 一周天气动态数据填充

--- js/function.js
+++ js/function.js
@@ -3,10 +3,17 @@ function removeIput() {
     document.getElementsByTagName('input')[0].value = '';
 }
 
-//jsonp方式通过客户端ip取得本地天气
-function loadWeatherByIP(_ip) {
+//jsonp方式通过客户端ip取得实况天气
+function loadTodayWeatherByIP(_ip) {
     let script = document.createElement("script");
     script.src = 'https://www.tianqiapi.com/api?' + 'version=v6&ip=' + _ip + '&callback=initNowWeather';
+    document.body.insertBefore(script, document.body.firstChild);
+}
+
+//jsonp方式通过客户端ip取得七天天气
+function loadWeekWeatherByIP(_ip) {
+    let script = document.createElement("script");
+    script.src = 'https://www.tianqiapi.com/api?' + 'version=v1&ip=' + _ip + '&callback=initWeekWeather';
     document.body.insertBefore(script, document.body.firstChild);
 }
 
@@ -80,13 +87,50 @@ function initNowWeather(response) {
     document.getElementById('now_high_tem').innerText = response.tem1;
 }
 
+//生成一周天气数据
+function initWeekWeather(response) {
+    //console.log(response);
+    //生成wea_img数组，
+    let logo_arr=response.data.map((item)=>{
+        return item.wea_img;
+    })
+
+    //生成温度数组，
+    let high_tem_arr=response.data.map((item)=>{
+        return parseInt(item.tem1.slice(0,-1));
+    })
+    let low_tem_arr=response.data.map((item)=>{
+        return parseInt(item.tem2.slice(0,-1));
+    })
+
+    //生成风况数组
+    let win_arr=response.data.map((item)=>{
+        return '风'+item.win_speed;
+    })
+
+    //生成空气质量数组
+    let air_arr=response.data.map((item)=>{
+        return item.air_level;
+    })
+
+    //计算yAxis中min,max
+    let lowest_tem=Math.min.apply(null,low_tem_arr);
+    let highest_tem=Math.max.apply(null,high_tem_arr);
+    let gap=highest_tem-lowest_tem;
+    let _min=lowest_tem-(gap*2);
+    let _max=highest_tem+(gap*3);
+
+    //console.log(logo_arr,high_tem_arr,low_tem_arr,win_arr,air_arr);
+    createWeekEchart(logo_arr,high_tem_arr,low_tem_arr,win_arr,air_arr,_min,_max);
+}
+
 //刷新页面后载入sessionStorage保存数据
 function pageReload() {
     let _data = JSON.parse(sessionStorage.getItem('wea_json'));
     if (_data) {
         for (let i = 0; i < _data.length; i++) {
             let footer = document.getElementsByTagName('footer')[0];
-            let node=createCityNode(_data[i]);
+            let node = createCityNode(_data[i]);
             footer.appendChild(node);
         }
     }
@@ -149,4 +193,51 @@ function addNewCity(response) {
     //取得数据
     let node = createCityNode(response);
     footer.appendChild(node);
+}
+
+//取得当前星期
+function getWeekArray() {
+    let result = [];
+    let weeks = new Array("周日", "周一", "周二", "周三", "周四", "周五", "周六");
+    let now = new Date();
+    let index = now.getDay();
+    //判断index值
+    //index为0,
+    if (index == 0) {
+        result = ['今天', '明天(周一)', '周二', '周三', '周四', '周五', '周六'];
+    } else {
+        for (let i = index; i < weeks.length; i++) {
+            result.push(weeks[i]);
+        }
+
+        for (let j = 0; j < index; j++) {
+            result.push(weeks[j]);
+        }
+        //替换数组前两个值
+        result[0] = '今天';
+        let temp = '明天(' + result[1] + ')';
+        result[1] = temp;
+    }
+    return result;
+}
+
+//取得一周日期
+function get7Day() {
+    let result = [];
+    for (let i = 0; i < 7; i++) {
+        let today = new Date();
+        let targetday_milliseconds = today.getTime() + 1000 * 60 * 60 * 24 * i;
+        today.setTime(targetday_milliseconds); //注意，这行是关键代码
+        let tMonth = today.getMonth() + 1;
+        let tDate = today.getDate();
+        if (tMonth < 10) {
+            tMonth = '0' + tMonth;
+        }
+
+        if (tDate < 10) {
+            tDate = '0' + tDate;
+        }
+        result.push(tMonth + "/" + tDate);
+    }
+    return result;
 }
