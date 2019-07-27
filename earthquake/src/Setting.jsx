4212cfb0aeec518ea364eeaf1f3646c5e6323ff9
blitheanon <blitheanon@qq.com>
2019-07-27 19:21:54 +0800

    Feature: 地震震级参数判断

--- earthquake/src/Setting.jsx
+++ earthquake/src/Setting.jsx
@@ -6,7 +6,6 @@ import {Link} from 'react-router-dom';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 
-
 //单选框组件
 import {makeStyles} from '@material-ui/core/styles';
 import Radio from '@material-ui/core/Radio';
@@ -26,7 +25,6 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
-
 const Setting = (props) => {
     const [open, setOpen] = React.useState(false);
 
@@ -36,10 +34,9 @@ const Setting = (props) => {
     // const [sort,setSort] = React.useState('新数据在前');
     // const [minMag,setMinMag] = React.useState('4');
     // const [maxMag,setMaxMag] = React.useState('10');
-    const [sort,setSort] = React.useState(props.orderby);
-    const [minMag,setMinMag] = React.useState(props.minMag);
-    const [maxMag,setMaxMag] = React.useState(props.maxMag);
-
+    const [sort, setSort] = React.useState(props.orderby);
+    const [minMag, setMinMag] = React.useState(props.minMag);
+    const [maxMag, setMaxMag] = React.useState(props.maxMag);
 
     function handleChange(event) {
         //设置地震排序sort
@@ -47,11 +44,25 @@ const Setting = (props) => {
     }
 
     function handleMinMagInput(event) {
-        setMinMag(event.target.value);
+        //正则表达式判断震级输入
+        let reg = /^([1-9]|10)$/;
+        if (reg.test(event.target.value)) {
+            setMinMag(event.target.value);
+        } else {
+            setMinMag('');
+        }
     }
 
     function handleMaxMagInput(event) {
-        setMaxMag(event.target.value);
+        //正则表达式判断震级输入
+        let reg = /^([1-9]|10)$/;
+        if (reg.test(event.target.value)) {
+            //设置地震排序sort
+            setMaxMag(event.target.value);
+        } else {
+            //设置地震排序sort
+            setMaxMag('');
+        }
     }
 
     function handleClickOpen() {
@@ -62,11 +73,16 @@ const Setting = (props) => {
         setOpen(false);
     }
 
-    function clickBack() {
-        //回传个性化地震参数
-        props.handleSettingData(sort,minMag,maxMag);
+    function clickBack(event) {
+        //判断最小震级是否小于最大震级
+        if (minMag <= maxMag && minMag !== '' && maxMag !== '') {
+            //回传个性化地震参数
+            props.handleSettingData(sort, minMag, maxMag);
+        } else {
+            event.preventDefault();
+            alert('请检查震级输入')
+        }
     }
-
 
     return (<div>
         <div id="title_bar">
@@ -77,18 +93,23 @@ const Setting = (props) => {
             <span id="placehold">占位</span>
         </div>
         <ul className="list">
-            <li className="setting-item" onClick={handleClickOpen}>排列方式<span>{sort==='time'?'新数据在前':'旧数据在前'}</span></li>
+            <li className="setting-item" onClick={handleClickOpen}>排列方式<span>{
+                        sort === 'time'
+                            ? '新数据在前'
+                            : '旧数据在前'
+                    }</span>
+            </li>
             <li className="setting-item">最小震级<input type="text" name="minmag" placeholder="请输入1-10的整数" value={minMag} onChange={handleMinMagInput}/></li>
             <li className="setting-item">最大震级<input type="text" name="maxmag" placeholder="请输入1-10的整数" value={maxMag} onChange={handleMaxMagInput}/></li>
         </ul>
         <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
             <DialogContent>
-                    <FormControl component="fieldset" className={classes.formControl}>
-                        <RadioGroup aria-label="gender" name="gender1" className={classes.group} value={sort} onChange={handleChange}>
-                            <FormControlLabel value="time" control={<Radio color="default"/>} label="新数据在前"/>
-                            <FormControlLabel value="time-asc" control={<Radio color="default"/>} label="旧数据在前"/>
-                        </RadioGroup>
-                    </FormControl>
+                <FormControl component="fieldset" className={classes.formControl}>
+                    <RadioGroup aria-label="gender" name="gender1" className={classes.group} value={sort} onChange={handleChange}>
+                        <FormControlLabel value="time" control={<Radio color = "default" />} label="新数据在前"/>
+                        <FormControlLabel value="time-asc" control={<Radio color = "default" />} label="旧数据在前"/>
+                    </RadioGroup>
+                </FormControl>
             </DialogContent>
         </Dialog>
     </div>)
